897d7d744f331fefab064ca1f5e092dd283d4a59
brunocarvalho-x <bhsprofissional@gmail.com>
2024-03-31 10:23:37 -0300

    feat: make the bootstrap and jquery work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+//= require jquery
+
+import '@hotwired/turbo-rails';
+import 'controllers';
+
+$(document).on('click', function () {
+  console.log('jquery está funcionando');
+});
