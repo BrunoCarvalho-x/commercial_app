3ce4ef43750a8c38a94c4004dc56bb698609e53e
brunocarvalho-x <bhsprofissional@gmail.com>
2024-03-30 22:12:30 -0300

    feat: test Order Model

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe 'Associations' do
+    #  it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:client_name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:delivery_date) }
+    it { is_expected.to validate_presence_of(:delivery_hour) }
+    it { is_expected.to validate_presence_of(:value) }
+  end
+end
