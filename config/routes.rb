1b5ed991d5d85fbade7460f2b775e8e25314e0c9
brunocarvalho-x <bhsprofissional@gmail.com>
2024-03-31 17:03:57 -0300

    feat: add the controller routes of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :orders
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :orders
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
