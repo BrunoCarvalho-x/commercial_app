32e088448285e219579624380aa9fdbbfe8d0fad
brunocarvalho-x <bhsprofissional@gmail.com>
2024-04-01 02:00:20 -0300

    feat: add more test to order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe 'Associations' do
-    #  it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:user) }
   end
 
   describe 'Validations' do
